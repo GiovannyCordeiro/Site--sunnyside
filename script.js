d8afffc316109be4cfdb33ba712de9075132a7b6
GiovannyCordeiro <94568582+GiovannyCordeiro@users.noreply.github.com>
2022-03-27 08:54:07 -0300

    erros de sintaxe e "melhoramento" do responsivo

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ const listNav = document.getElementById('listNav');
 menuHamburguer.addEventListener('click',()=>{
     listNav.classList.toggle('active')
 })
+
+
+/* function soma (num1, num2){
+    let resultsoma = num1 + num2;
+    return resultsoma;
+};
+
+console.log(soma(19, 9)) */
